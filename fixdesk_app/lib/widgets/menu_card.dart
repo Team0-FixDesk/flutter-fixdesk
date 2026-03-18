0c49898eaa0cc077d0c198bdfc4e1bd0adee99fa
Nattaphat2004 <nattaphathmm12@gmail.com>
2026-03-18 14:13:25 +0700

    edit v2

---
+++ fixdesk_app/lib/widgets/menu_card.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class AppHeader extends StatelessWidget {
+  const AppHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
+      color: Colors.white,
+      child: Row(
+        children: [
+
+          /// LOGO
+          Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const Icon(
+              Icons.build,
+              color: Colors.white,
+              size: 20,
+            ),
+          ),
+
+          const SizedBox(width: 10),
+
+          /// TEXT FIXDESK
+          Text(
+            "FixDesk",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
